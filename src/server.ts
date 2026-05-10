88e729159938b49a5b497194c75b8e3ec76b5ea1
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-11 00:08:26 +0600

    how to get http responses using node method

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,28 @@
 import { createServer, IncomingMessage, Server } from "http";
 
 const server: Server = createServer((req: IncomingMessage, res) => {
-    console.log(req);
+
+    // console.log(req.url); // * '/', '/user', '/products'
+    // console.log(req.method); // * "GET", "POST", "DELETE"
+
+    const url = req.url;
+    const method = req.method;
+
+    if (url === "/" && method === "GET") {
+        // console.log("This is root route");
+
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "This is root route" }));
+
+        // ! The server sends a response string or buffer to the client, not an object. Because the server does not send the response in the form of an object to the client side.
+
+    } else if (url?.startsWith("/products")) {
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "This is products route" }));
+    } else {
+        res.writeHead(404, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "Route not found" }));
+    }
 });
 
 server.listen(5000, () => {
