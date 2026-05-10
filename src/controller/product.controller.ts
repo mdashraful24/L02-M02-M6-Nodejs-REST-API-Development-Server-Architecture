028481bd12d8bc86270e326a8742e1c58c311ae2
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-11 02:38:46 +0600

    added get method explore and understood concepts

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,11 +1,17 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController = (req: IncomingMessage, res: ServerResponse) => {
 
     const url = req.url;
     const method = req.method;
 
+    const urlParts = url?.split("/");
+    const id = urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+    // console.log("This is the actual id: ", id);
+
+    // * get all products
     if (url === "/products" && method === "GET") {
 
         // const products = [
@@ -20,6 +26,17 @@ export const productController = (req: IncomingMessage, res: ServerResponse) => 
         res.end(JSON.stringify({
             message: "Products retrieved successfully",
             data: products
+        }));
+    } else if (method === "GET" && id !== null) {
+        // * get single product
+
+        const products = readProduct();
+        const product = products.find((p: IProduct) => p.id === id);
+
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({
+            message: "Product retrieved successfully",
+            data: product
         }));
     }
 };
---
+++ src/types/product.type.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    category: string;
+    brand: string;
+    price: number;
+    stock: number;
+    rating: number;
+}
