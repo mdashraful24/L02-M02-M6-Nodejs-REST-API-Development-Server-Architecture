f9592ed2aec0930b5b0f9d778206507472f679be
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-11 20:36:28 +0600

    added executing delete method operation

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -33,6 +33,13 @@ export const productController = async (req: IncomingMessage, res: ServerRespons
 
         const products = readProduct();
         const product = products.find((p: IProduct) => p.id === id);
+        if (!product) {
+            res.writeHead(404, { "content-type": "application/json" });
+            return res.end(JSON.stringify({
+                message: "Product not found!",
+                data: product
+            }));
+        }
 
         res.writeHead(200, { "content-type": "application/json" });
         res.end(JSON.stringify({
@@ -63,6 +70,7 @@ export const productController = async (req: IncomingMessage, res: ServerRespons
             data: newProduct
         }))
     } else if (method === "PUT" && id !== null) {
+        // * Put products
 
         const body = await parseBody(req);
         const products = readProduct();
@@ -90,6 +98,27 @@ export const productController = async (req: IncomingMessage, res: ServerRespons
         res.end(JSON.stringify({
             message: "Product updated successfully",
             data: products[index]
+        }))
+    } else if (method === "DELETE" && id != null) {
+        const products = readProduct();
+        const index = products.findIndex((p: IProduct) => p.id === id)
+
+        if (index < 0) {
+            res.writeHead(404, { "content-type": "application/json" });
+            return res.end(JSON.stringify({
+                message: "Product not found",
+                data: null
+            }))
+        }
+
+        products.splice(index, 1);
+        // console.log(products);
+        insertProduct(products);
+
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({
+            message: "Product deleted successfully",
+            data: null
         }))
     }
 };
